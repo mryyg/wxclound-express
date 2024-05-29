122a9e10e6128eaf9dc53a6ae982b3adf90425ff
yangyongguang <mac@macdeMacBook-Pro.local>
2024-05-29 16:02:25 +0800

    openRouter

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
 
 const logger = morgan("tiny");
 
@@ -18,28 +17,44 @@ app.get("/", async (req, res) => {
 });
 
 // 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
+app.post("/api/opneRouter", async (req, res) => {
+  const { messages } = req.body;
+  try {
+    const streamData = await fetch(
+      "https://openrouter.ai/api/v1/chat/completions",
+      {
+        method: "POST",
+        headers: {
+          Authorization:
+            "Bearer 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",
+          "Content-Type": "application/json",
+          Accept: "text/event-stream",
+        },
+        body: JSON.stringify({
+          model: "openchat/openchat-7b:free",
+          messages,
+          stream: true,
+        }),
+      }
+    ).then(async (res) => {
+      return res.ok ? res.body : null;
     });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
+    // console.log(streamData);
+    res.writeHead(200, {
+      "Content-Type": "text/event-stream",
+    });
 
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
+    await (async () => {
+      for await (const value of streamData) {
+        res.write(value);
+      }
+    })();
+    res.end();
+    // res.send({
+    //   code: 200,
+    //   data: 11,
+    // });
+  } catch (error) {}
 });
 
 // 小程序调用，获取微信 Open ID
@@ -52,7 +67,6 @@ app.get("/api/wx_openid", async (req, res) => {
 const port = process.env.PORT || 80;
 
 async function bootstrap() {
-  await initDB();
   app.listen(port, () => {
     console.log("启动成功", port);
   });
